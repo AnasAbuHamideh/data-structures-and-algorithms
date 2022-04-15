d0c277c00b549b3fbf83d53825779c1a6a546af1
AnasAbuHamideh <anas.abuhamida@gmail.com>
2022-04-15 04:57:15 +0300

    first commit

--- linked-list/linked-list/linked_list/linked_list.py
+++ linked-list/linked-list/linked_list/linked_list.py
@@ -1,7 +1,54 @@
+class Node:
+
+    def __init__(self,value):
+       self.value = value
+       self.next = None
+
 class LinkedList:
 
     def __init__(self):
         self.head = None
 
-    def append(self,Node):
-        pass
+    def insert(self, node):
+        if self.head is None:
+            self.head = node
+
+        else:
+            current = self.head
+            while current.next is not None:
+                current = current.next
+            current.next = node
+
+        
+    def includes(self, value):
+      current=self.head
+      while current and current.next != None:
+          if current.value == value:
+              return True
+          current=current.next
+      return False
+
+
+    def toString(self):
+        output = ""
+        if self.head is None:
+            output = "Empty linked list"
+        else:
+            current = self.head
+            while(current):
+                output+= f'[{current.value}] -->'
+                current = current.next
+            
+            output+= "None"
+        return output
+    
+
+if __name__ == "__main__" :
+    li = LinkedList()
+    node1 = Node("1")
+    node2 = Node("2")
+    li.insert(node1)
+    li.insert(node2)
+    print(li.includes("1"))
+    print(li.toString())
+    
--- linked-list/linked-list/tests/test_linked_list.py
+++ linked-list/linked-list/tests/test_linked_list.py
@@ -1,5 +1,43 @@
-from linked_list import __version__
+from linked_list.linked_list import Node,LinkedList
 
+def test_Linked_List():
+    li = LinkedList()
+    node1 = Node(1)
+    node2 = Node(2)
+    li.insert(node1)
+    li.insert(node2)
+    actual = li.head.value
+    expected = 1
+    assert actual == expected
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_Linked_List_insert():
+    li = LinkedList()
+    node1 = Node("A")
+    node2 = Node("B")
+    li.insert(node1)
+    li.insert(node2)
+    actual = li.head.value
+    expected = "A"
+    assert actual == expected
+
+def test_Linked_List_includes():
+    li = LinkedList()
+    node1 = Node(1)
+    node2 = Node(2)
+    li.insert(node1)
+    li.insert(node2)
+    actual = li.includes(1)
+    expected = True
+    assert actual == expected
+
+def test_Linked_List_toString():
+    li = LinkedList()
+    node1 = Node("A")
+    node2 = Node("B")
+    node3 = Node("C")
+    li.insert(node1)
+    li.insert(node2)
+    li.insert(node3)
+    actual = li.toString()
+    expected = "[A] -->[B] -->[C] -->None"
+    assert actual == expected
