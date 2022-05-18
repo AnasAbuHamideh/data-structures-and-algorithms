adfffcc636d65d2e4706b72fccd550cb12c5f665
AnasAbuHamideh <anas.abuhamida@gmail.com>
2022-05-18 22:31:50 +0300

    first commit

--- insertion_sort/tests/test_insertion_sort.py
+++ insertion_sort/tests/test_insertion_sort.py
@@ -1,13 +1,29 @@
-def insertion_Sorts(arr):
-    i=1
-    for i in range(len(arr)):
+from insertion_sort.inserton import insertion_Sorts
 
-       j = i-1
-       temp=arr[i]
 
-       while j >= 0 and temp < arr[j]:
-           arr[j+1]=arr[j]
-           j=j-1
+def test_insertion_1():
+    arr = [8,4,23,42,16,15]
+    expected = [4,8,15,16,23,42]
+    actual = insertion_Sorts(arr)
+    assert actual == expected
 
-       arr[j+1]=temp
-    return arr
+def test_insertion_2():
+    arr = [20,18,12,8,5,-2]
+    expected = [-2, 5, 8, 12, 18, 20]
+    actual = insertion_Sorts(arr)
+    assert actual == expected
+    
+
+def test_insertion_3():
+    arr = [5,12,7,5,5,7]
+    expected = [5, 5, 5, 7, 7, 12]
+    actual = insertion_Sorts(arr)
+    assert actual == expected
+    
+
+def test_insertion_4():
+    arr = [2,3,5,7,13,11]
+    expected = [2, 3, 5, 7, 11, 13]
+    actual = insertion_Sorts(arr)
+    assert actual == expected
+    
